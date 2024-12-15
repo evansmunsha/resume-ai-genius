0e889c1310dd8f7a333236b27e92b5bb1014c768
evansmunsha <evansensteen@gmail.com>
2024-12-15 05:06:08 +0200

    "data"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
 import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
-import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { constructMetadata } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = constructMetadata()
+/* export const metadata: Metadata = {
   title: {
     template: "%s - Resume AI Genius",
     absolute: "Resume AI Genius",
   },
   description:
     "Resume AI Genius is the easiest way to create a professional resume that will help you land your dream job.",
-};
+}; */
 
 export default function RootLayout({
   children,
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,7 @@ import { twMerge } from "tailwind-merge";
 import { CoverLetterServerData } from "./types/types";
 import { ResumeValues, CoverLetterValues } from "./validation";
 import { ResumeServerData } from "./types";
+import { Metadata } from "next";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -107,3 +108,45 @@ export function mapToCoverLetterValues(data: CoverLetterServerData): CoverLetter
 
 
 
+export function constructMetadata({
+  title = 'Build Your Dream Resume & Cover Letter in Just Minutes',
+  description = 'Resume AI Genius is the easiest way to create a professional resume that will help you land your dream job. plus a Cover Letter.',
+  image = '/opengraph-image.jpeg',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@evansensteen',
+    },
+    icons,
+    metadataBase: new URL('https://resume-ai-genius.vercel.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
+}
