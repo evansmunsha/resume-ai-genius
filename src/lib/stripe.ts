c90bbbd561eff08c6d5c270be80dd760de5ba14d
evansmunsha <evansensteen@gmail.com>
2025-01-04 00:07:04 +0200

    "stripe"

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,9 +1,6 @@
-import Stripe from "stripe";
 import { env } from "@/env";
+import Stripe from "stripe";
 
-const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-10-28.acacia",
-  typescript: true,
-});
+const stripe = new Stripe(env.STRIPE_SECRET_KEY);
 
 export default stripe;
